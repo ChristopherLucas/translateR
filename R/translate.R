8891e7d19bfe8759d7de7d755b1311b5231ad71d
ChristopherLucas <clucas@fas.harvard.edu>
2014-04-30 00:12:46 -0400

    Added round trip translation function

--- R/translate.R
+++ R/translate.R
@@ -1,16 +1,33 @@
 library(RCurl)
 library(RJSONIO)
+library(stringdist)
+
+print.translateClass <- function(x){
+    print(x$translated.text)
+}
 
 translate <- function(to.translate, source.lang, target.lang, key){
     base <- 'https://www.googleapis.com/language/translate/v2?'
-    key <- paste('key=', key, sep = '')
+    key.str <- paste('key=', key, sep = '')
     query <- paste('&q=', curlEscape(to.translate), sep = '')
     source.str <- paste('&source=', source.lang, sep = '')
     target.str <- paste('&target=', target.lang, sep = '')
-    api.url <- paste(base, key, query, source.str, target.str, sep = '')
+    api.url <- paste(base, key.str, query, source.str, target.str, sep = '')
 
     translated <- fromJSON(getURL(api.url))$data$translations[[1]]
     translated <- unname(translated)
-    return(translated)
+
+    out <- list(translated.text = translated, source.text = to.translate,
+                source.lang = source.lang, target.lang = target.lang, key = key)
+    class(out) <- 'translateClass'
+    return(out)
 }
 
+rtt <- function(t.obj){
+    original.text <- tolower(t.obj$source.text)
+    print(original.text)
+    translated.back <- tolower(translate(t.obj$translated.text, t.obj$target.lang,
+                                         t.obj$source.lang, t.obj$key)$translated.text)
+    lv.dist <- stringdist(original.text, translated.back, 'lv')
+    return(lv.dist)
+}
