bf6923190cbe8b29519e9503b9330266eced34c4
ChristopherLucas <clucas@fas.harvard.edu>
2014-05-27 12:07:14 -0400

    Split queries

--- R/translate.R
+++ R/translate.R
@@ -66,8 +66,8 @@ querySplit <- function(query){
     print(nchar(query))
     for(i in seq(0, nchar(query), 1000)){
         start.index <- which.min(abs(start.and.finish$start - i))
-        string.vec <- c(string.vec, query[prev.end:start.and.finish$start[start.index]])
-        prev.end <- start.and.finish$end[i]
+        string.vec <- c(string.vec, substr(query, prev.end, start.and.finish$start[start.index]))
+        prev.end <- start.and.finish$end[start.index]
     }
     return(string.vec)
 }
