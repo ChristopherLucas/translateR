3b5c3f11af1217ad4e82e4d0e9cca901e1b74258
ChristopherLucas <clucas@fas.harvard.edu>
2014-06-30 22:02:47 -0400

    Added language codes

--- R/language_codes.R
+++ R/language_codes.R
@@ -1,7 +1,5 @@
-# Class for objects returned by translate()
-print.translateClass <- function(x){
-    print(x$translated.text)
-}
+# Functions to print the language codes for
+# the Google and Microsoft Translation APIs
 
 getMicrosoftLanguages <- function(){
     cat("
@@ -143,36 +141,4 @@ Yiddish:                'yi'
 Yoruba:                 'yo'
 Zulu:                   'zu'
 ")
-}
-
-translate <- function(dataset = NULL, content.field = NULL, content.vec = NULL,
-                      api.key, translator = 'Google', source.lang, target.lang){
-    
-}
-
-translate <- function(to.translate, source.lang, target.lang, key){
-    out <- translateText(to.translate, source.lang, target.lang, key)
-    class(out) <- 'translateClass'
-    return(out)
-}
-
-translateText <- function(to.translate, source.lang, target.lang, key){
-    to.translate.original <- to.translate
-    translated <- gTranslate(to.translate, source.lang, target.lang, key)
-    out <- list(translated.text = translated, source.text = to.translate.original,
-                source.lang = source.lang, target.lang = target.lang, key = key)
-    return(out)
-}
-
-gTranslate <- function(to.translate, source.lang, target.lang, key){
-    base <- 'https://www.googleapis.com/language/translate/v2?'
-    key.str <- paste('key=', key, sep = '')
-    query <- paste('&q=', curlEscape(to.translate), sep = '')
-    source.str <- paste('&source=', source.lang, sep = '')
-    target.str <- paste('&target=', target.lang, sep = '')
-
-    api.url <- paste(base, key.str, query, source.str, target.str, sep = '')
-    
-    translated <- fromJSON(getURL(api.url))$data$translations[[1]]
-    return(translated)
 }
