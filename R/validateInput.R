2815b5cb5045a0cab39514793866d569cd452409
ChristopherLucas <clucas@fas.harvard.edu>
2014-07-02 04:11:33 -0400

    Split off validateInput

---
+++ R/validateInput.R
@@ -0,0 +1,43 @@
+validateInput <- function(dataset, content.field, content.vec, api.key, translator, source.lang, target.lang){
+    if(is.character(api.key) == FALSE){
+        stop("api.key must be a character type.")
+    }
+    if((is.null(dataset) | is.null(content.field)) & is.null(content.vec)){
+        stop("You must either provide both a dataset and the content field or a single vector of content.")
+    }
+    if(!(translator %in% c('Google', 'Microsoft'))){
+        stop("You must select either 'Google' or 'Microsoft' as the translator.")
+    }
+    if(translator == 'Google'){
+        if(!(source.lang %in% unname(unlist(languages['Google'])))){
+            msg <- paste("The source.lang '", source.lang, "' is not a valid Google language code. To see a list of Google language codes, use getGoogleLanguages().", sep = '')
+            stop(msg)
+        }
+        if(!(target.lang %in% unname(unlist(languages['Google'])))){
+            msg <- paste("The target.lang '", target.lang, "' is not a valid Google language code. To see a list of Google language codes, use getGoogleLanguages().", sep = '')
+            stop(msg)
+        }
+    }    
+    if(translator == 'Microsoft'){
+        if(!(source.lang %in% unname(unlist(languages['Microsoft'])))){
+            msg <- paste("The source.lang '", source.lang, "' is not a valid Microsoft language code. To see a list of Microsoft language codes, use getMicrosoftLanguages().", sep = '')
+            stop(msg)
+        }
+        if(!(target.lang %in% unname(unlist(languages['Microsoft'])))){
+            msg <- paste("The target.lang '", target.lang, "' is not a valid Microsoft language code. To see a list of Microsoft language codes, use getMicrosoftLanguages().", sep = '')
+            stop(msg)
+        }
+    }
+    if(!(is.null(dataset))){
+        if(!(is.data.frame(dataset))){stop("dataset must be a data.frame.")}
+    }
+    if(!(is.null(content.field))){
+        if(!(is.character(content.field))){stop("content.field must be a character.")}
+    }
+    if(!(is.null(content.field))){
+        if(!(is.character(dataset[[content.field]]))){stop("The column containing the content must be a character vector.")}
+    }
+    if(!(is.null(content.vec))){
+        if(!(is.character(content.vec))){stop("content.vec must be a character vector.")}
+    }
+}
