f8ebd4ca5909d52270cdcc7d21794a0095e3b544
Richard Cotton <richie@Richards-MacBook-Air.local>
2021-02-23 23:12:45 -0500

    use v3 API

--- R/microsoftTranslate.R
+++ R/microsoftTranslate.R
@@ -1,13 +1,50 @@
+# See documentation at 
+# https://docs.microsoft.com/en-us/azure/cognitive-services/translator/reference/v3-0-translate
 microsoftTranslate <-
-function(x, api.key, source.lang, target.lang){
-  
-    params = paste("text=", URLencode(x), "&to=", target.lang, "&from=", source.lang, sep = '')
-    translateUrl = paste("http://api.microsofttranslator.com/v2/Http.svc/Translate?", params, sep = '')
+function(x, api.key, source.lang = NULL, target.lang){
+    checkText(x)
+    microsoft_langs <- languageCodes()$Microsoft
+    if(!is.null(source.lang)) {
+      source.lang <- match.arg(source.lang, microsoft_langs)
+    }
+    target.lang <- match.arg(target.lang, microsoft_langs)
     
-    return(
-        cleanFun(
-            GET(translateUrl, add_headers("Ocp-Apim-Subscription-Key" = api.key))
-            )
-        )
+    base_url <- "https://api.cognitive.microsofttranslator.com/translate"
+    body <- data.frame(Text = x)
+    query = list(
+      "api-version" = "3.0",
+      to = target.lang,
+      from = source.lang
+    )
+    response <- POST(
+      base_url, 
+      body = body,
+      add_headers(
+        "Ocp-Apim-Subscription-Key" = api.key,
+        "Content-Type" = "application/json; charset=UTF-8"
+      ),
+      query = query,
+      encode = "json"
+    )
+    httr::stop_for_status(response)
+    contents <- httr::content(response)
+    # This is cleaner with purrr, but extra dependency is overkill for 1 line
+    # translations <- map_chr(contents, pluck, "translations", 1, "text")
+    translations <- vapply(
+      contents,
+      function(x) x[["translations"]][[1]][["text"]],
+      character(1L)
+    )
+    return(translations)
+    contents
 
+}
+
+checkText <- function(x) {
+  if(length(x) > 100) {
+    stop("A maximum of 100 strings can be translated at once.")
+  }
+  if(sum(nchar(x)) > 10000) {
+    stop("A maixmum of 10000 characters can be translated at once.")
+  }
 }
